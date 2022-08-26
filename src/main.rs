c94513d3e07c46bf32414fb32c63a2b7d6adb91c
Anatoly Yakovenko <anatoly@solana.com>
2022-08-26 12:24:58 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,19 @@ fn main() {
     let mut num_partitions = 0;
     let mut partition_slot = 0;
     const TIME: usize = 128;
-    let mut once = false;
     for slot in 0..TIME * 1000 {
         network.step();
         println!("root {:?}", network.root());
-        if num_partitions == 0 && slot >= TIME && slot % TIME == 0 && !once {
+        if num_partitions == 0 && slot >= TIME && slot % TIME == 0 {
             println!("CREATING PARTITIONS===================================");
             network.create_partitions(2);
             partition_slot = slot;
             num_partitions = 2;
         }
-        if num_partitions > 0 && partition_slot + TIME / 4 == slot && !once {
+        if num_partitions > 0 && partition_slot + TIME / 4 == slot {
             println!("REPAIRING PARTITIONS=================================");
             network.repair_partitions();
             num_partitions = 0;
-            once = true;
         }
     }
 }
