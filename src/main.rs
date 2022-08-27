9bec9f446636ce658701fd8aa88d9195e977a8b5
Anatoly Yakovenko <anatoly@solana.com>
2022-08-26 21:49:21 -0700

    udpate

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,16 @@ use tower_sim::network;
 fn main() {
     let mut network = network::Network::default();
     let mut num_partitions = 1;
-    const TIME: usize = 128;
+    const TIME: usize = 256;
     let mut partition_slot = 0;
-    let mut once = false;
     for slot in 0..TIME * 1000 {
         network.step();
         println!("root {:?}", network.root());
-        if !once && num_partitions == 1 && slot >= TIME && slot % TIME == 0 {
+        if num_partitions == 1 && slot >= TIME && slot % TIME == 0 {
             println!("CREATING PARTITIONS===================================");
             network.create_partitions(2);
-            num_partitions = 2;
+            num_partitions = 4;
             partition_slot = slot;
-            once = true;
         }
         if num_partitions > 1 && partition_slot + TIME / 8 <= slot && slot % (TIME / 8) == 0 {
             println!("REPAIRING PARTITIONS=================================");
